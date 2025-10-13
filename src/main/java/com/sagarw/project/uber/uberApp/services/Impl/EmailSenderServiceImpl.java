9e3ead9c67bd84191cd0d160b6e319d30955822d
Sagar Wadhwa <sagarwadhwa726@yahoo.com>
2025-10-13 23:03:53 +0530

    Added Email sending support

---
+++ src/main/java/com/sagarw/project/uber/uberApp/services/Impl/EmailSenderServiceImpl.java
@@ -0,0 +1,47 @@
+package com.sagarw.project.uber.uberApp.services.Impl;
+
+import com.sagarw.project.uber.uberApp.services.EmailSenderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailSenderServiceImpl implements EmailSenderService {
+
+    /*when the configuration is done in application.properties java mail sender will be autoconfigured, we will have a from Email*/
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void sendEmail(String toEmail, String subject, String body) {
+        try{
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setTo(toEmail);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(body);
+            javaMailSender.send(simpleMailMessage);
+            log.info("Email has been sent !");
+        }
+        catch (Exception e) {
+            log.info("Can't send e-mail : {}", e.getMessage());
+        }
+    }
+
+    @Override
+    public void sendEmail(String[] toEmail, String subject, String body) {
+        try{
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setTo(toEmail);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(body);
+            javaMailSender.send(simpleMailMessage);
+            log.info("Email has been sent !");
+        }
+        catch (Exception e) {
+            log.info("Can't send e-mail : {}", e.getMessage());
+        }
+    }
+}
